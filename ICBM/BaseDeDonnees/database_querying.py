1d91d0a888ef87199f269687b19531f246407989
Samuel Comeau <samuel.comeau.3@ulaval.ca>
2020-06-02 14:59:00 -0400

    Work in progress: API request can now receive several request without problems occurring in the sessions.

--- ICBM/BaseDeDonnees/database_querying.py
+++ ICBM/BaseDeDonnees/database_querying.py
@@ -1,18 +1,24 @@
 from ICBM.BaseDeDonnees.data_tables import *
 from ICBM.database_setup import *
 
-session = Session()
-
 
 def get_facteur_climatique(region):
-    return session.query(FacteurClimatique).filter(FacteurClimatique.region == region).one()
+    session = Session()
+    query_result = session.query(FacteurClimatique).filter(FacteurClimatique.region == region).one()
+    session.close()
+    return query_result
 
 
 def get_facteur_series_de_sol(series_de_sol):
-    return session.query(FacteurSerieDeSol).filter(FacteurSerieDeSol.serie_de_sol == series_de_sol).one()
+    session = Session()
+    query_result = session.query(FacteurSerieDeSol).filter(FacteurSerieDeSol.serie_de_sol == series_de_sol).one()
+    session.close()
+    return query_result
 
 
 def get_coefficients_des_residus_de_culture(culture_principale):
-    return session.query(CoefficientDesResidusDeCulture).filter(
+    session = Session()
+    query_result = session.query(CoefficientDesResidusDeCulture).filter(
         CoefficientDesResidusDeCulture.culture_principale == culture_principale).one()
-
+    session.close()
+    return query_result
